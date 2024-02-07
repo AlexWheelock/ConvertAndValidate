21d5fa71220d29e7467211584f33417b1dc2679c
AlexWheelock <130195109+AlexWheelock@users.noreply.github.com>
2024-02-06 18:22:57 -0700

    Add project files.

---
+++ ConvertAndValidate/ConvertAndValidate.vb
@@ -0,0 +1,16 @@
+﻿'Alex Wheelock
+'RCET 0265
+'Spring 2024
+'
+
+Option Strict On
+Option Explicit On
+
+Module ConvertAndValidate
+
+    Sub Main()
+
+        Console.Read()
+    End Sub
+
+End Module
